07c9c3331c532d0dde4ee828da103a91fa233a73
Clara Prando <clara@macbook-air-de-clara.home>
2024-04-08 14:53:43 +0200

    tripouilletester

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: claprand <claprand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 13:08:10 by claprand          #+#    #+#             */
-/*   Updated: 2024/04/08 14:34:40 by claprand         ###   ########.fr       */
+/*   Updated: 2024/04/08 14:52:13 by claprand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,6 @@ char	*ft_strrchr(const char *s, int c)
 	int	i;
 
 	i = 0;
-	if (s[i] == '\0')
-		return (NULL);
 	while (s[i])
 		i++;
 	while (i >= 0)
